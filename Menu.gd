643246d39b0aba4c636c066e436a956f31c5d223
ojmaster <jafferyo.123@gmail.com>
2024-10-06 13:53:15 -0500

    add main menu

---
+++ Menu.gd
@@ -0,0 +1,10 @@
+extends Control
+
+func _on_play_pressed() -> void:
+	get_tree().change_scene_to_file("res://World.tscn")
+
+func _on_controls_pressed() -> void:
+	pass # Replace with function body
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()
