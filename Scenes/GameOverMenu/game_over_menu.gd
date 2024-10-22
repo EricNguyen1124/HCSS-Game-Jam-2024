351d46537bc6cfb42b13b3fedcad7564a3d4c9c1
Eric Nguyen <47893402+EricNguyen1124@users.noreply.github.com>
2024-10-21 21:24:35 -0500

    new leaderboard

--- Scenes/GameOverMenu/game_over_menu.gd
+++ Scenes/GameOverMenu/game_over_menu.gd
@@ -5,18 +5,44 @@ class_name GameOverMenu extends Control
 @onready var quit_button: Button = $QuitButton
 @onready var leaderboard_scene: PackedScene = preload("res://addons/silent_wolf/Scores/Leaderboard.tscn")
 
+@onready var name_line_edit: LineEdit = $NameLineEdit
+@onready var submit_leaderboard_button: Button = $SubmitButton
+
+var leaderboard_name: String = ""
+var score_submitted: bool = false
+var leaderboard
+
 func _ready() -> void:
 	retry_button.button_down.connect(retry_game)
 	quit_button.button_down.connect(back_to_menu)
 
+	name_line_edit.text_changed.connect(on_text_changed)
+	name_line_edit.text_submitted.connect(submit_score)
+	submit_leaderboard_button.button_down.connect(on_submit_score_button)
+
+	leaderboard = leaderboard_scene.instantiate()
+	add_child(leaderboard)
+
+func _process(_delta: float) -> void:
+	submit_leaderboard_button.disabled = leaderboard_name.is_empty() or score_submitted
+
 func set_values(rings, zombies, score):
+	PlayerVariables.final_score = score
 	scoreUI.set_values(rings, zombies, score)
 
-	if !PlayerVariables.player_name.is_empty() and score != 0:
-		await SilentWolf.Scores.save_score(PlayerVariables.player_name, score).sw_save_score_complete
-		
-	var leaderboard = leaderboard_scene.instantiate()
-	add_child(leaderboard)
+func on_text_changed(text: String) -> void:
+	leaderboard_name = text
+
+func on_submit_score_button() -> void:
+	submit_score(leaderboard_name)
+
+func submit_score(player_name: String) -> void:
+	score_submitted = true
+	await SilentWolf.Scores.save_score(player_name, PlayerVariables.final_score).sw_save_score_complete
+	if leaderboard:
+		leaderboard.queue_free()
+		leaderboard = leaderboard_scene.instantiate()
+		add_child(leaderboard)
 
 func retry_game() -> void:
 	get_tree().change_scene_to_file("res://World.tscn")
